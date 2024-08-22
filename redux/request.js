31bfd1b3d6d9cf18cce447b0e687716b0906b849
TahoorAhmed2 <tahoorahmed@softsyncdev.com>
2024-08-22 20:14:47 +0500

    first commit

--- redux/request.js
+++ redux/request.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // const BASE_URL = `http://localhost:8080/api`;
 const BASE_URL =
-  process.env.NEXT_PUBLIC_BASE_URL || `http://localhost:8080/api`;
+  process.env.NEXT_PUBLIC_BASE_URL || `https://4devari-api.vercel.app/api`;
 
 const request = axios.create({ baseURL: BASE_URL });
 
